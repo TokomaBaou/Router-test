663a0e927bea7265e768e376d9cab4908ed57d7b
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-19 23:44:43 +0900

    デバック

--- src/router/Page1Routes.jsx
+++ src/router/Page1Routes.jsx
@@ -2,12 +2,12 @@ import { Page1 } from "../Page1";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
 
-export const Page1Routes = [
+export const page1Routes = [
   {
     path: "/",
     exact: true,
     children: <Page1 />
   },
-  { path: "/page1", exact: false, children: <Page1DetailA /> },
-  { path: "/page2", exact: false, children: <Page1DetailB /> }
+  { path: "/detailA", exact: false, children: <Page1DetailA /> },
+  { path: "/detailB", exact: false, children: <Page1DetailB /> }
 ];
